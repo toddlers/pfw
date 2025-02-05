05fbf73f28c8002ce37d11ddeb63885618fe269d
Suresh Kumar <er.sureshprajapati@gmail.com>
2025-02-05 18:10:30 +0100

    using arc for thread safety

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,28 @@
-use std::io::{Read, Write};
-use std::net::{TcpListener, TcpStream};
+use std::io::{BufReader, BufWriter, Read, Write};
+use std::net::{Shutdown, TcpListener, TcpStream};
 use std::{io, thread};
 extern crate simplelog;
 use simplelog::*;
 use std::env;
+use std::sync::Arc;
 
-fn forward(mut from: TcpStream, mut to: TcpStream) -> io::Result<()> {
+fn forward(from: Arc<TcpStream>, to: Arc<TcpStream>) -> io::Result<()> {
+    let mut reader = BufReader::new(from.as_ref());
+    let mut writer = BufWriter::new(to.as_ref());
+    //Vec::with_capacity(4096) creates an empty vector,
+    // but read(&mut buffer) requires the buffer to have pre-allocated space.
     let mut buffer = [0; 4096];
     loop {
-        let bytes_read = from.read(&mut buffer)?;
-        log::info!("Read {} bytes", bytes_read);
-
+        let bytes_read = reader.read(&mut buffer)?;
         if bytes_read == 0 {
             break;
         }
-        to.write_all(&buffer[..bytes_read])?;
+        writer.write_all(&buffer[..bytes_read])?;
+        writer.flush()?;
     }
+    log::info!("Closing connection");
+    //BufWriter may hold data in its buffer instead of immediately sending it.
+    let _ = to.shutdown(Shutdown::Both); // Properly close the socket
     Ok(())
 }
 
@@ -23,12 +30,22 @@ fn handle_client(client_stream: TcpStream, target_addr: &str) -> io::Result<()> 
     match TcpStream::connect(target_addr) {
         Ok(server_stream) => {
             log::info!("Forwarding connection to {}", target_addr);
-            let client = client_stream.try_clone()?;
-            let server = server_stream.try_clone()?;
-            let _ = thread::spawn(move || forward(client, server));
-            let _ = thread::spawn(move || forward(server_stream, client_stream));
+            // use Arc more safer for thread safe
+            let client = Arc::new(client_stream);
+            let server = Arc::new(server_stream);
+            let client_clone = Arc::clone(&client);
+            let server_clone = Arc::clone(&server);
+            // why two threads:
+            // bi-directional flow
+            // concurrency
+            // non blocking
+            // scored threads for clean exit, no leftover
+            // thread::spawn -> non blocking threads, but may leave lingering threads
+            // thread::scope -> safer, but blocks  the funciton forver, but cleaned up automatically
+            thread::spawn(move || forward(client, server));
+            thread::spawn(move || forward(server_clone, client_clone));
         }
-        Err(e) => log::error!("Connection failed: {}", e),
+        Err(e) => log::error!("Can not connect to {} : {}", target_addr, e),
     }
     Ok(())
 }
@@ -54,7 +71,8 @@ fn main() -> io::Result<()> {
 
     let listener = TcpListener::bind(&src_connection_string)?;
     log::info!(
-        "Port forwarder running on 127.0.0.1:3003 -> {}",
+        "Port forwarder running on 127.0.0.1:{} -> {}",
+        src_port,
         dst_connection_string
     );
     for stream in listener.incoming() {
