92682222214a4a328ed8ee9346b42c3510998909
Suresh Kumar <er.sureshprajapati@gmail.com>
2025-02-05 17:08:18 +0100

    accept ports from the commandline

--- README.md
+++ README.md
@@ -10,4 +10,4 @@ python3 -m http.server 3004
 
 2. `cargo run` inside this dir, this forwards the port from 3003 to 3004
 
-3. run a simple curl command like `curl http://localhost:3003
+3. run a simple curl command like `curl http://localhost:3003`
--- src/main.rs
+++ src/main.rs
@@ -2,8 +2,8 @@ use std::io::{Read, Write};
 use std::net::{TcpListener, TcpStream};
 use std::{io, thread};
 extern crate simplelog;
-
 use simplelog::*;
+use std::env;
 
 fn forward(mut from: TcpStream, mut to: TcpStream) -> io::Result<()> {
     let mut buffer = [0; 4096];
@@ -34,6 +34,14 @@ fn handle_client(client_stream: TcpStream, target_addr: &str) -> io::Result<()> 
 }
 
 fn main() -> io::Result<()> {
+    let args: Vec<String> = env::args().collect();
+    if args.len() < 3 {
+        eprintln!("Usage: {} <from> <to> ", args[0]);
+        std::process::exit(1);
+    }
+    let src_port = &args[1];
+    let dst_port = &args[2];
+
     CombinedLogger::init(vec![TermLogger::new(
         LevelFilter::Info,
         Config::default(),
@@ -41,16 +49,18 @@ fn main() -> io::Result<()> {
         ColorChoice::Auto,
     )])
     .unwrap();
-    let listener = TcpListener::bind("127.0.0.1:3003")?;
-    let target_addr = "127.0.0.1:3004";
+    let src_connection_string = format!("127.0.0.1:{}", src_port);
+    let dst_connection_string = format!("127.0.0.1:{}", dst_port);
+
+    let listener = TcpListener::bind(&src_connection_string)?;
     log::info!(
         "Port forwarder running on 127.0.0.1:3003 -> {}",
-        target_addr
+        dst_connection_string
     );
     for stream in listener.incoming() {
         match stream {
             Ok(client_stream) => {
-                let target_addr = target_addr.to_string();
+                let target_addr = dst_connection_string.to_string();
                 thread::spawn(move || handle_client(client_stream, &target_addr));
             }
             Err(e) => log::error!("Connection failed: {}", e),
